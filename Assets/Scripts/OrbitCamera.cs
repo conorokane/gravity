360608b4f85918ed9b5d6fa8e3225fef4e8abcde
Conor O'Kane <cokane@cokane.com>
2025-11-30 17:56:50 +1100

    Improved camera and lensing shader

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -9,36 +9,38 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField] private float orbitSpeed, lookSpeed, zoomSpeed;
     [SerializeField] private Vector2 orbitClampX, lookClampX, zoomClamp;
 
-    void Update()
+    private Vector2 orbitDrag = Vector2.zero, lookDrag = Vector2.zero;
+    private float dt = 0;
+
+    void LateUpdate()
     {
-        Vector2 orbitDrag = Vector2.zero;
-        Vector2 lookDrag = Vector2.zero;
+        orbitDrag = Vector2.zero;
+        lookDrag = Vector2.zero;
+        dt = Time.deltaTime;
 
         // read mouse drags
         if (Input.GetMouseButton(0))
         {
-            orbitDrag = new Vector2(-Input.mousePositionDelta.x, Input.mousePositionDelta.y);
+            orbitDrag = new Vector2(Input.mousePositionDelta.x, -Input.mousePositionDelta.y);
+            orbitX.Rotate(orbitSpeed * dt * Vector3.right * orbitDrag.y);
+            orbitY.Rotate(orbitSpeed * dt * Vector3.up * orbitDrag.x);
+            float orbitXRotation = MathUtils.ConstrainAngle(orbitX.localEulerAngles.x);
+            orbitX.localEulerAngles = new Vector3(Mathf.Clamp(orbitXRotation, orbitClampX.x, orbitClampX.y), 0, 0);
         }
         else if (Input.GetMouseButton(1))
         {
             lookDrag = new Vector2(Input.mousePositionDelta.x, -Input.mousePositionDelta.y);
+            lookX.Rotate(lookSpeed * dt * Vector3.right * lookDrag.y);
+            lookY.Rotate(lookSpeed * dt * Vector3.up * lookDrag.x);
+            float lookXRotation = MathUtils.ConstrainAngle(lookX.localEulerAngles.x);
+            lookX.localEulerAngles = new Vector3(Mathf.Clamp(lookXRotation, lookClampX.x, lookClampX.y), 0, 0);
         }
 
-        // apply rotations
-        orbitX.Rotate(orbitSpeed * Time.deltaTime * Vector3.right * orbitDrag.y);
-        orbitY.Rotate(orbitSpeed * Time.deltaTime * Vector3.up * orbitDrag.x);
-        lookX.Rotate(lookSpeed * Time.deltaTime * Vector3.right * lookDrag.y);
-        lookY.Rotate(lookSpeed * Time.deltaTime * Vector3.up * lookDrag.x);
-
-        // clamp rotation
-        float orbitXRotation = MathUtils.ConstrainAngle(orbitX.localEulerAngles.x);
-        float lookXRotation = MathUtils.ConstrainAngle(lookX.localEulerAngles.x);
-
-        orbitX.localEulerAngles = new Vector3(Mathf.Clamp(orbitXRotation, orbitClampX.x, orbitClampX.y), 0, 0);
-        lookX.localEulerAngles = new Vector3(Mathf.Clamp(lookXRotation, lookClampX.x, lookClampX.y), 0, 0);
-
         //zoom
-        zoomPivot.Translate(zoomSpeed * Time.deltaTime * Vector3.forward * Input.mouseScrollDelta.y * Mathf.Abs(zoomPivot.localPosition.z));
-        zoomPivot.localPosition = new Vector3(0, 0, Mathf.Clamp(zoomPivot.localPosition.z, zoomClamp.x, zoomClamp.y));
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            zoomPivot.Translate(zoomSpeed * dt * Vector3.forward * Input.mouseScrollDelta.y * Mathf.Abs(zoomPivot.localPosition.z));
+            zoomPivot.localPosition = new Vector3(0, 0, Mathf.Clamp(zoomPivot.localPosition.z, zoomClamp.x, zoomClamp.y));
+        }
     }
 }
